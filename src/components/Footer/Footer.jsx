4539ef9bac1693efd6437e4669a3d51dba3d0a4f
DeaDelphine <dealbadelphine@gmail.com>
2024-06-06 14:33:31 +0200

    modify footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router-dom"
+import Cookies from "./Cookies"
 
 
 function Footer() {
@@ -6,11 +8,11 @@ function Footer() {
       <div className="container border-top">
         <div className="row">
           <div className="col-13 col-md">
-            <h5>Asso'S Et Vous</h5><small className="d-block text-muted">&copy;</small>
+            <h5>Asso'S Et Vous &copy;</h5><small className="d-block text-muted"></small>
           </div>
           <div className="col-6 col-md">
             <h5>Nous Suivre</h5>
-            <ul className="list-unstyled">
+            <ul className="list-unstyled d-flex">
               <li>
                 <a href="#" className="text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
@@ -29,12 +31,12 @@ function Footer() {
 
             </ul>
           </div>
-          <div className="col-6 col-md">
+          <div className="col-6 col-md d-flex">
             <ul className="list-unstyled">
-              <li><a href="contact.php" className="text-muted">Gestion des Cookies</a></li>
               <li><a href="#" className="text-muted">Mentions légales</a></li>
               <li><a href="#" className="text-muted">Politique de confidentialité</a></li>
               <li><a href="#" className="text-muted">Conditions générales</a></li>
+              <li><a href="#" className="text-muted">Gestion des Cookies</a></li>
             </ul>
           </div>
         </div>
